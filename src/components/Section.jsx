ff5b9403a639c483716218bb6ff5ddd56ebfc9aa
KJJDSA <kjjdsa@gmail.com>
2025-07-06 21:33:05 +0900

    나머지 매개변수를 사용한 패턴으로 props 넘기기

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,9 @@
 // 구조를 만드는 재사용 가능한 컴포넌트를 만들 수도 있다. 
 
-export default function Section({ title, id, children }) {
+export default function Section({ title, children, ...props }) {
   return ( // id 속성 또한 props로 전달받는다.
-    <section id={id}> 
+    // props를 통해 전달받은 속성들을 spread 연산자로 전달하는 패턴으로 모든 속성을 전달하는 번거로움을 피할 수 있다. 
+    <section {...props}> 
       <h2>{title}</h2>
       {children}
     </section>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,8 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) { // 나머지 매개변수 : https://ko.javascript.info/rest-parameters-spread
   console.log('TABBUTTON COMPONENT EXECUTING');
   return (
     <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+      <button className={isSelected ? 'active' : undefined} {...props}>
         {children}
       </button>
     </li>
