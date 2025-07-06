728c455588c7df21c63d97db6a4d7fa6001457c0
KJJDSA <kjjdsa@gmail.com>
2025-07-06 21:13:22 +0900

    스테이트별 컴포넌트 분리 학습 주석

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header /> {/* 이제 state로 컴포넌트가 분리되었기 때문에 Examples 가 변해도 앞부분이 변하지 않는다. */}
       <main>
         <CoreConcepts />
         <Examples />
