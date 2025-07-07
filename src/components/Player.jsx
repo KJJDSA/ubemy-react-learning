467ff78c81a5b12460397022f603c95e1f380764
KJJDSA <kjjdsa@gmail.com>
2025-07-07 22:55:49 +0900

    첫번 째 블록(유저) : Edit 버튼 만들기 1(정답)

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,13 +3,22 @@ import { useState } from 'react';
 export default function Player({name, symbol}) {
   const [isEditing, setIsEditing] = useState(false);
 
+  function handleEditClick() {
+    setIsEditing((prev) => !prev);
+  }
+  
+  // jsx 에서 분기하지 않고 변수로 지정한 뒤 if 문을 사용했다. 간결하고 이후 변경된 이름을 저장할 때 value 를 뽑아 사용할 때도 더 편리할 것 같다.
+  let playerName = <span className="player-name">{name}</span>
+  if (isEditing) {
+    playerName = <input type="text" defaultValue={name} />;
+  }
+
   return (
     <li>
       <span className="player">
-        {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" defaultValue={name}/>}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button onClick={() => setIsEditing((prev) => !prev)}>{isEditing ? "Save" : "Edit"}</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span> 
     </li>
 )
