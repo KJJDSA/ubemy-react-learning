41d252b8f2c66f388a38094c51ce83658ed2e449
KJJDSA <kjjdsa@gmail.com>
2025-07-11 22:13:48 +0900

    vanilla css 사용의 단점

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <>
       <Header />
+      {/* 바닐라css 단점: css 파일을 분리해도 스코프되지 않는다.(Header.jsx 바깥의 header 태그에 똑같이 적용되는 모습) */}
+      <header><h1>good</h1></header>
       <main>
         <AuthInputs />
       </main>
