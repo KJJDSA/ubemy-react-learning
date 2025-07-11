6e9c42c8361d9ad71b2e7614b64928c038d085a8
KJJDSA <kjjdsa@gmail.com>
2025-07-11 22:24:51 +0900

    인라인 스타일 의 장단점

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
     <>
       <Header />
       {/* 바닐라css 단점: css 파일을 분리해도 스코프되지 않는다.(Header.jsx 바깥의 header 태그에 똑같이 적용되는 모습) */}
-      <header><h1>good</h1></header>
+      <header><h1>이제 여기에는 스타일이 적용되지 않음</h1></header>
       <main>
         <AuthInputs />
       </main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,17 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      {/* 인라인 스타일 장점: 빠르고 쉽게 JSX 에 스타일을 부여할 수 있음 / 단점: 모든 요소를 개별적으로 스타일해아함*/}
+      <h1 style={{
+        fontSize: "1.5rem",
+        fontWeight: "600",
+        letterSpacing: "0.4em",
+        textAlign: "center",
+        textTransform: "uppercase",
+        color: "#9a3412",
+        fontFamily: ['Pacifico', "cursive"],
+        margin: "0"
+      }}>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
