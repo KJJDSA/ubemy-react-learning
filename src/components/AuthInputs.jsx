144fcab78db5747787b417063b5d42d408adf637
KJJDSA <kjjdsa@gmail.com>
2025-07-14 20:59:28 +0900

    tailwind) styled component에서 전환하기 1

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,17 +1,6 @@
 import { useState } from 'react';
-import {styled} from 'styled-components'
-import Button from "./Button.jsx"
-import CustomInput from "./Input.jsx"
-
-/* 컴포넌트를 감싸는 래퍼 styled component. 이 파일 외에 쓰일 일이 없으므로 따로 파일로 빼는것은 불필요한 작업이다. 
-하지만 input, button 등은 다른 곳에서도 재사용이 가능하므로 파일로 빼둘 수 있다. */
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
-
+import Button from './Button'
+import Input from './Input'
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -34,48 +23,27 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs" className={"bg-[red]"}>
+    <div id="auth-inputs">
       {/* styled components 를 사용해 하나의 태그 리터럴 템플릿을 생성하면, 그것에 대해 고유한 클래스를 부여하고 적용된 태그들에게 해당 클래스를 부여함   */}
-      <ControlContainer >
-        <CustomInput 
-          label={"Email"}
-          invalid={emailNotValid}
-          type="email"
-          onChange={(event) => handleInputChange('email', event.target.value)}
-        />
-        <CustomInput 
-          label={"Password"}
-          invalid={passwordNotValid}
-          type="password"
-          onChange={(event) => handleInputChange('password', event.target.value)}
-        />
-        {/* <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
+      <div >
+        <Input
+            invalid={emailNotValid}
+            label='Email'
             type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
-            // }}
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p> */}
-        {/* <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
+            onChange={(event) => handleInputChange('email', event.target.value)}/>
+        <Input
+            invalid={passwordNotValid} 
+            label='Password'
             type="password"
-            $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p> */}
-      </ControlContainer>
+              handleInputChange('password', event.target.value)}
+        />
+      </div>
       <div className="actions">
-        <Button type="button" className="text-button">
+        <Button type="button" >
           Create a new account
         </Button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,33 +1,3 @@
-import {styled} from "styled-components"
-
-// .button, .text-button 클래스 둘 다 button 태그를 사용하고 있으므로 합칠 수 있다.
-// &가상선택자, 중첩기능을 한번 더 활용하면 각각의 클래스에 맞는 스타일을 한 styled component에 집약할 수 있다.
-const Button = styled.button`
-  &.button {
-    padding: 1rem 2rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    border-radius: 0.25rem;
-    color: #1f2937;
-    background-color: #f0b322;
-    border-radius: 6px;
-    border: none;
-    
-    &:hover {
-      background-color: #f0920e;
-    }
-  }
-
-  &.text-button {
-    color: #f0b322;
-    border: none;
-
-    &:hover {
-      color: #f0920e;
-    }
-  }
-
-  /* ... 아래에 해당하는 클래스를 추가하면 또 사용할 수 있을 것이다.  */
-`
-
-export default Button;
+export default function Button({children, ...props}) {
+  return <button className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-amber-400 hover:bg-amber-500" {...props}>{children}</button>
+}
