9fc9bb10cc5101c37edb2b7a6844f20a963d54c9
KJJDSA <kjjdsa@gmail.com>
2025-07-17 17:19:24 +0900

    타이머 챌린지: 변수만 사용했을 경우

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import {useState, useRef} from 'react'
+
 
 const TimerChallenge = ({title, targetTime}) => {
+  let timer; // 타이머를 담는 변수이다. 재 렌더링시 초기화된다.
+  const [timerStarted, setTimerStarted]= useState(false)
+  const [timerExpired, setTimerExpired]= useState(false)
+
+  function handleStart() {
+    timer = setTimeout(() => {
+      setTimerExpired(true)
+    }, targetTime * 1000)
+
+    setTimerStarted(true)
+  }
+
+  function handleStop(){
+    console.log(timer) // undefined
+    clearTimeout(timer) //  초기화된 timer를 clear해봤자 타이머는 막지 못한다. you lose! 
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired&& <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>
+        <button onClick={timerStarted? handleStop : handleStart}>
           Stop Challenge
         </button>
       </p>
-      <p className="">
-        Time is running
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Timer is running...' : 'Timer inactive'}
       </p>
     </section>
   )
