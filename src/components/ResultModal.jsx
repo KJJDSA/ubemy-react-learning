bb45c1ae691a0d221a8275254bab551bab250623
KJJDSA <kjjdsa@gmail.com>
2025-07-17 23:31:55 +0900

    ResultModal 을 최상위 div 로 올리기(createPortal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 /* 19버전 이상의 리액트에서는 이렇게 ref값을 넘기지만, 그 이전 리액트에서는 아래의 방법을 사용한다. */
 // import {forwardRef} from 'react';
@@ -17,7 +18,7 @@ const ResultModal = ({ ref, targetTime, remainingTime, onReset }) => {
       dialog.current.showModal();
     },
   }));
-  return (
+  return createPortal(
     // dialog 는 esc 키를 사용해서 닫을 수도 있다. 이 때 onReset 이 작동하도록 하기 위해선 dialog 의 onClose 내장속성을 사용하면 된다.
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {/* dialog 는 기본적으로 보이지 않으며 open 속성으로 강제로 보이게 하면 백그라운드가 흐려지지 않게된다.*/}
@@ -34,7 +35,8 @@ const ResultModal = ({ ref, targetTime, remainingTime, onReset }) => {
       <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById('modal'),
   );
 };
 // )
