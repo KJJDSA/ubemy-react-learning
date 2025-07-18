f5f0f6d39d2709b0aa983dbab76498355a3deef2
KJJDSA <kjjdsa@gmail.com>
2025-07-18 17:06:24 +0900

    챌린지: 사이드바 UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import SideBar from "./components/SideBar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <main className="h-screen my-8 flex gap-8">
+        <SideBar />
+        Intro, Create, Tasks
+      </main>
+    </>
+  );
 }
 
 export default App;
