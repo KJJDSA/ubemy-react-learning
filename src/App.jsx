c9f4e2fe6bc69e0e1552cc8dbc9c33aa30b07076
KJJDSA <kjjdsa@gmail.com>
2025-07-19 18:34:22 +0900

    NoProjectSelected 컴포넌트 작업

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,20 @@ function App() {
   const [isCreateFormOn, setIsCreateFormOn] = useState(false);
   const [projectList, setPrejectList] = useState([]);
 
+  // 스위치 모드를 넣는것이 좋을까(아래 조건으로 분기하는 handleSwich)? 아니면 저장란에 state를 변경하면 알아서 UI가 그려지게 하는게 좋을까.
+  function handleSwich(mode) {
+    if (mode === "create") {
+      setIsCreateFormOn(true);
+    }
+  }
+
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
         <SideBar />
         {/* selectedProjectNumber 가 null 이면 NoProjectSelected 를 보여준다*/}
         {selectedProjectNumber === null && !isCreateFormOn && (
-          <NoProjectSelected />
+          <NoProjectSelected onSwich={handleSwich} />
         )}
         {/* selectedProjectNumber 가 null 이 아니면 NoProjectSelected 를 보여준다*/}
         {selectedProjectNumber !== null && !isCreateFormOn && (
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Image from "../assets/no-projects.png";
 
-const NoProjectSelected = () => {
-  return <div className="bg-amber-100">NoProjectSelected</div>;
+const NoProjectSelected = ({ onSwich }) => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img className="w-16 h-16 object-contain mx-auto" src={Image} />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        선택된 프로젝트가 없습니다
+      </h2>
+      <p className="text-stone-400 mb-4">
+        프로젝트를 선택하거나 새로운 프로젝트를 생성하세요
+      </p>
+      <button
+        className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
+        onClick={() => onSwich("create")}
+      >
+        신규 프로젝트 생성
+      </button>
+    </div>
+  );
 };
 
 export default NoProjectSelected;
