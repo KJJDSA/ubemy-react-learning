c792e97cc6f50f747cceb994743106d951db4d3a
KJJDSA <kjjdsa@gmail.com>
2025-07-20 15:54:28 +0900

    해설강의 피드백: 같은 속성의 반복은 스프레드문법 사용하기

--- src/components/CreateFormInput.jsx
+++ src/components/CreateFormInput.jsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const CreateFormInput = ({
   children,
-  type,
-  value,
   onChangeKey,
   onChange,
   validation,
+  ...props
 }) => {
   const inputClassName = `w-full h-20 p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600${
     !validation && " border-red-300 bg-red-200"
@@ -22,17 +21,15 @@ const CreateFormInput = ({
         {type === "longText" && (
           <input
             className={inputClassName}
-            type={type}
-            value={value}
             onChange={(event) => onChange(onChangeKey, event.target.value)}
+            {...props}
           />
         )}
         {type !== "longText" && (
           <input
             className={inputClassName}
-            type={type}
-            value={value}
             onChange={(event) => onChange(onChangeKey, event.target.value)}
+            {...props}
           />
         )}
       </label>
