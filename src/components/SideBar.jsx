65256c578ace5a31256243e8530fefca9f958730
KJJDSA <kjjdsa@gmail.com>
2025-07-20 18:15:14 +0900

    해설강의 피드백: .map() 메소드 jsx 안에서 더 자유롭게 사용하기

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,19 +10,22 @@ const SideBar = ({ projectList, onSwich, currentIndex }) => {
       {/* 버튼같은 작은요소부터 재사용성을 끌어올릴 수 있어야 한다. onClick 속성은 꼭 넘어가서 정의하지 않아도 동작한다. */}
       <Button onClick={() => onSwich("create")}>+ 프로젝트 만들기</Button>
       <ul className="mt-8">
-        {projectList.map((project, index) => (
-          <li key={project.title + index}>
-            <button
-              key={project.title + index}
-              onClick={() => onSwich("project", index)}
-              className={`w-full text-left px-6 py-2 rounded-md text-stone-50 hover:bg-stone-800${
-                currentIndex === index ? " bg-stone-800" : "bg-stone-900"
-              }`}
-            >
-              {project.title}
-            </button>
-          </li>
-        ))}
+        {projectList.map((project, index) => {
+          // 이렇게하면 편리하게 코드를 넣을 수 있다.
+          const className = `w-full text-left px-6 py-2 rounded-md text-stone-50 hover:bg-stone-800${
+            currentIndex === index ? " bg-stone-800" : "bg-stone-900"
+          }`;
+          return (
+            <li key={project.title + index}>
+              <button
+                onClick={() => onSwich("project", index)}
+                className={className}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
