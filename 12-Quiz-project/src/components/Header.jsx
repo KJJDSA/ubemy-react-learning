34dd5cbdb12759912bacdc1cb2bfe9d42f910927
KJJDSA <kjjdsa@gmail.com>
2025-07-24 17:18:24 +0900

    챕터 12: (안보고 해보기 1 피드백) 적절한 alt 적기

--- 12-Quiz-project/src/components/Header.jsx
+++ 12-Quiz-project/src/components/Header.jsx
@@ -3,7 +3,7 @@ import HeaderImg from "../assets/quiz-logo.png";
 const Header = () => {
   return (
     <header>
-      <img src={HeaderImg} alt="" />
+      <img src={HeaderImg} alt="Quiz Logo" />
       <h1>React Quiz</h1>
     </header>
   );
