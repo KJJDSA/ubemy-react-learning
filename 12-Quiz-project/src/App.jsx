7bd22164bcab1f323dc1a22b4829c7f1906310b0
KJJDSA <kjjdsa@gmail.com>
2025-07-24 18:52:41 +0900

    챕터 12: questions.js 추가. 규칙 주석

--- 12-Quiz-project/src/App.jsx
+++ 12-Quiz-project/src/App.jsx
@@ -1,5 +1,10 @@
 import Header from "./components/Header";
-
+/** Quiz App
+ * Rule:
+ *  1. questions.js 에는 질문 배열이 있으며, id, 질문, 객관식 답 배열이 있다.
+ *  2. 객관식 답 배열의 첫번째 요소가 정답이다.
+ *  3. 객관식 답은 출력될 때 순서를 랜덤으로 정렬해야 한다.
+ */
 function App() {
   return (
     <>
