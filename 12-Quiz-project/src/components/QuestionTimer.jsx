478cf6617ca220db57ae3ac725ccb5781ea15bfe
KJJDSA <kjjdsa@gmail.com>
2025-07-28 18:08:13 +0900

    챕터 12: (안보고 해보기3) 문제를 선택해도 타이머가 수정되지않는 문제 해결하기

--- 12-Quiz-project/src/components/QuestionTimer.jsx
+++ 12-Quiz-project/src/components/QuestionTimer.jsx
@@ -4,35 +4,24 @@ const QuestionTimer = ({ timeoutSecond, onTimeout }) => {
   const timeout = timeoutSecond * 1000;
   const [remainingTime, setRemainingTime] = useState(timeout);
 
-  /* interval주기마다 새로운 interval이 생성되는것을 막기 위해, useEffect를 사용하여 재실행되지 않게 해준다. */
+  /* onTimeout에 useEffect가 반응할 때 마다 setInterval 도 초기화되도록 한다. */
+  /* interval 주기마다 새로운 timeout 과 interval 이 생성되는 것을 막기 위해, useEffect으로 감싸준다. */
   useEffect(
     () => {
+      console.log("setting setTimeout");
+      const timeoutTimer = setTimeout(() => {
+        onTimeout(null);
+      }, timeout);
+
+      console.log("setting setInterval");
       const intervalTimer = setInterval(() => {
         setRemainingTime((prev) => [prev - 100]);
       }, 100);
 
       return () => {
-        clearInterval(intervalTimer);
-      };
-    },
-    [] /* 의존성이 비어있으므로 Effect가 재실행되지 못한다. */
-  );
-
-  /* interval 주기마다 새로운 timeout 이 생성되는 것을 막기 위해, useEffect으로 감싸준다. */
-  useEffect(
-    () => {
-      const timeoutTimer = setTimeout(() => {
-        onTimeout(null);
-        
-        /* 
-        문제가 끝나고 타이머가 다 줄어든 후에도 이 remainingTime의 값은 계속 유지된다. 
-        remainingTime 을 초기화 해주어야 한다. 
-        */
-        setRemainingTime(timeout);
-      }, timeout);
-
-      return () => {
         clearTimeout(timeoutTimer);
+        clearInterval(intervalTimer);
+        setRemainingTime(timeout);
       };
     },
     [
